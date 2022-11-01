412f132657cec0cad3544aba72ad852bd6982a65
Arkadiy2907 <arkadiy2907@gmail.com>
2022-11-01 18:36:41 +0300

    ver0

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Game from "./сomponents/Game/Game"
+import './scss/style.scss';
+
+
+
+function App() {
+  return (
+    <>
+      <div className="wrap">
+        <Game />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
